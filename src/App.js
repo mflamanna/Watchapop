e997359c2c0885f2bc2e00ac8df4dcb5157a62fc
nuria-barea <nuriab@gmail.com>
2022-07-21 12:57:41 +0200

    Design of detail product main done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ListWatch from './components/ListWatch';
 import NavBar from './components/NavBar';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
 
 function App() {
   return (
     <>
     <NavBar/>
     <ListWatch/>
-    
     </>
   );
 }
--- src/application/router.js
+++ src/application/router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import App from '../App';
+import Detail from '../components/Detail';
 
 function Router (){
 return (
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<App/>} />
+            <Route path='/detail' element={<Detail/>} />
         </Routes>
     </BrowserRouter>
     )
