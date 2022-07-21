8c3a3f3c6a8a9191061a223388a9ffb34edee64b
RosaPs <rosapacheco.contact@gmail.com>
2022-07-21 11:01:47 +0200

    Footer done and working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ListWatch from './components/ListWatch';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
     <NavBar/>
     <ListWatch/>
+    <Footer/>
+
     
     </>
   );
