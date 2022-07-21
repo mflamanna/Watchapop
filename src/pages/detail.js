7b894fe764f7787e1342fce517dcc61210ccfa66
nuria-barea <nuriab@gmail.com>
2022-07-21 18:28:26 +0200

    Detail page in process

---
+++ src/pages/detail.js
@@ -0,0 +1,16 @@
+import NavBar from '../components/NavBar';
+import Detail from '../components/Detail';
+import Footer from'../components/Footer'; 
+
+function DetailPage (){
+    return (
+        
+        <>        
+        <NavBar/>
+        <Detail/>  
+        </>
+          
+        )
+    }
+
+    export default DetailPage;
---
+++ src/services/Getinfo.jsx
@@ -0,0 +1,10 @@
+import axios from "axios"
+
+const Getinfo= {   
+    async getAll(){  
+        const result = await axios.get (process.env.REACT_APP_API_URL)
+        return result.data    
+    }       
+}
+
+export default Getinfo;
