1dc3325fe67788f93deaaaf107504c529545defd
KevsCha <k-e-v-i-n_13_@hotmail.com>
2022-07-21 14:59:16 +0200

    123

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,7 +29,7 @@ x-data="{open:false,menu:false, lokasi:false}">
                   <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path>
                 </svg>
               </button>
-              <input type="text" name="s" id="s" className="block w-full pl-10 pr-3 py-2 border border-transparent rounded-md leading-5 bg-white-200 text-gray-300 placeholder-gray-400 focus:outline-none focus:bg-white focus:text-gray-900 sm:text-sm transition duration-150 ease-in-out" placeholder="Search"/>
+              <input type="text" name="s" id="searc" className="block w-full pl-10 pr-3 py-2 border border-transparent rounded-md leading-5 bg-white-200 text-gray-300 placeholder-gray-400 focus:outline-none focus:bg-white focus:text-gray-900 sm:text-sm transition duration-150 ease-in-out" placeholder="Search"/>
             </form>
           </div>
         </div>
@@ -55,5 +55,6 @@ x-data="{open:false,menu:false, lokasi:false}">
     </div>
   </div>
 </nav>
+
     )
 }
