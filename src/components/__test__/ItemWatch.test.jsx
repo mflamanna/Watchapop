be292bb09970bcac2e3e1039d4d78053f6652e14
Miguel <miguelsalvador.rguez@gmail.com>
2022-07-22 08:20:15 +0200

    Second hotfix, tests updated

--- src/components/__test__/ItemWatch.test.jsx
+++ src/components/__test__/ItemWatch.test.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { ReactDOM } from "react-dom";
-import SearchNavBar from "../SearchNavBar";
-
+import ItemWatch from "../ItemWatch";
 
 it("should render with crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<SearchNavBar/>, div);
+    ReactDOM.render(<ItemWatch/>, div);
 
 })
